aeb19ea299e7d20c979940552017202856c5a31c
LeninK8 <joslias1003@gmail.com>
2026-09-08 13:15:22 -0500

    ci: configure GitHub Pages deployment
    Add a workflow to automate deployment to GitHub Pages and update the Vite base path to ensure correct asset resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/flor-que-da-fulgor/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
